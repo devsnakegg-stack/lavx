3ba0423cad90af79d651e4733ab32d2ef95185b1
devsnakegg-stack <devsnakegg@gmail.com>
2026-02-02 09:52:55 +0530

    Delete src directory

--- src/Map.ts
+++
@@ -1,9 +0,0 @@
-export const PlatformMap: Record<string, string> = {
-  spotify: 'spsearch',
-  apple: 'amsearch',
-  deezer: 'dzsearch',
-  yandex: 'ymsearch',
-  youtube: 'ytsearch',
-  'youtube music': 'ytmsearch',
-  soundcloud: 'scsearch',
-};
